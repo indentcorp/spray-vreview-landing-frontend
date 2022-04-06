e3e8fce40aa7d5205c00c550a6deb3821c242246
Lee Jihye <0516ja@gmail.com>
2022-04-06 19:00:37 +0900

    Price 컴포넌트 구현

--- components/Price.tsx
+++ components/Price.tsx
@@ -1,14 +1,66 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { RelativeColors } from '@/theme/Colors'
+
 export const Price = () => {
   return (
     <Container>
-      <Text>Price</Text>
+      <Content>
+        <GuideText>
+          <Title>판매된 만큼만 <br/> 이용료 부과</Title>
+          <Description>
+            스프레이는 앱 내에서 발생하는 주문 건 대응과 관련한 <br/>
+            실제 서비스 이용 요금만 청구하고 있습니다.
+          </Description>
+          <SubDescription>* 최대 20%까지 청구될 수 있습니다</SubDescription>
+        </GuideText>
+        <ServiceFeeImage src="/assets/service-fee.png" />
+      </Content>
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.div`
+  padding: 0 32px;
+`
 
-const Text = styled.p``
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const GuideText = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Title = styled.p`
+  font-weight: 700;
+  font-size: 3rem;
+  line-height: 130%;
+  color: ${RelativeColors.gray0};
+`
+
+const Description = styled.p`
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 160%;
+  color: ${RelativeColors.gray400};
+  padding-top: 40px;
+`
+
+const SubDescription = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 140%;
+  color: ${RelativeColors.gray500};
+  padding-top: 12px;
+`
+
+const ServiceFeeImage = styled.img`
+  width: 403px;
+  height: 380px;
+`
