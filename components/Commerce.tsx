49dadef2098d2cc9becf8f2b7f1dc61b9a9170e2
devsehyeon <dev.blackbull@gmail.com>
2022-04-06 16:51:05 +0900

    Commerce component 레이아웃 구현

---
+++ components/Commerce.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Commerce = () => {
+  return (
+    <Container>
+      <Text>Commerce</Text>
+    </Container>
+  )
+}
+
+const Container = styled.div``
+
+const Text = styled.p``
