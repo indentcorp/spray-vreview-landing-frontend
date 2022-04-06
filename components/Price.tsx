d03f2187c75bab8a603884fc99827bd1e08ab41c
devsehyeon <dev.blackbull@gmail.com>
2022-04-06 16:52:12 +0900

    Price component 레이아웃 구현

---
+++ components/Price.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Price = () => {
+  return (
+    <Container>
+      <Text>Price</Text>
+    </Container>
+  )
+}
+
+const Container = styled.div``
+
+const Text = styled.p``
