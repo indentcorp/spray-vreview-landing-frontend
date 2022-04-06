bddf7c43093ca98f5ae598bd61de76f473acf88b
devsehyeon <dev.blackbull@gmail.com>
2022-04-06 23:49:59 +0900

    GA script 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,6 +60,15 @@ const Home: NextPage = () => {
       `,
           }}
         ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P443HV5');`,
+          }}
+        ></script>
       </Head>
       <Header />
       <IntroWrapper>
@@ -88,6 +97,16 @@ const Home: NextPage = () => {
         <Contact />
       </Section>
       <Footer />
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P443HV5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>`,
+        }}
+      />
     </>
   )
 }
