dbc646f32dc996d8b07aa546cbb3a1eb0b2829b1
devsehyeon <dev.blackbull@gmail.com>
2022-04-22 17:34:58 +0900

    TagManager initialize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import TagManager from 'react-gtm-module'
+declare var process: {
+  env: {
+    NEXT_PUBLIC_GTM_ID: string
+  }
+}
+
+const tagManagerArgs = {
+  gtmId: process.env.NEXT_PUBLIC_GTM_ID,
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
   return <Component {...pageProps} />
 }
 
